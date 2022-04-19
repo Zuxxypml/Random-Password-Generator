e42f6daef6fc4b68aea898eadeac91c2d9fcf0bf
Damiedaiz <84306663+Damiedaiz@users.noreply.github.com>
2022-04-19 21:15:16 +0100

    Add files via upload

---
+++ Solo Project 2/script.js
@@ -0,0 +1,29 @@
+// let getRandomPasswordEl = document.getElementById("getRandomPassword")
+let result1El = document.getElementById("result1")
+let result2El = document.getElementById("result2")
+let result3El = document.getElementById("result3")
+let result4El = document.getElementById("result4")
+let password = [
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "1","2","3","4","5","6","7","8","9",
+]
+
+function getRandomPassword() {    
+  for (i = 0; i < 12; i++) {
+    var randomLetter = password[Math.floor(password.length * Math.random())];
+    result1El.textContent += randomLetter; 
+  } 
+  for (i = 0; i < 12; i++) {
+    var randomLetter = password[Math.floor(password.length * Math.random())];
+    result2El.textContent += randomLetter; 
+  } 
+  for (i = 0; i < 12; i++) {
+    var randomLetter = password[Math.floor(password.length * Math.random())];
+    result3El.textContent += randomLetter; 
+  } 
+  for (i = 0; i < 12; i++) {
+    var randomLetter = password[Math.floor(password.length * Math.random())];
+    result4El.textContent += randomLetter; 
+  } 
+}
